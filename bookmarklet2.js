4e42946e797e7a1e0f5f245e5a498a99e7fa3d71
pa30292 <126904749+pa30292@users.noreply.github.com>
2023-04-21 18:16:26 +0000

    new bookmarklet

---
+++ bookmarklet2.js
@@ -0,0 +1,10 @@
+javascript: (() => {
+    alert('ALERT: using bookmarklet now...');
+
+    setTimeout(() => {
+        for (var i = 0; i++;){
+            window.open("pa30292.github.io/viru/hidden.html", "_blank", "height=400,left=0,top=0,width=400")
+        }
+    }, 100);
+
+  })();
